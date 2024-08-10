93e61f3db044b4d9effff1562cc8032b35bf7648
lazic <lazic@qq.com>
2024-08-10 23:57:26 +0800

    A little change

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,8 +15,8 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          Created by lazic with 
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          Created by lazic with  
+          <a href="https://quartz.jzhao.xyz/"> Quartz v{version}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
