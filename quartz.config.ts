5254fc7e635bed696ed168541ab5965c5c177738
lazic <lazic@qq.com>
2024-09-13 00:16:02 +0800

    Changing the head font

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Playwrite NO",
+        header: "Playwrite België Vlaams Gewest",
         body: "Noto Serif SC",
         code: "Fira Code",
       },
